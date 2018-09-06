8328334d1cd2549f617484780a6a3194f1a07faf
unknown <KrupchakAS@yandex.ru>
2018-09-06 16:41:54 +0300

    commit

--- src/main/java/app/controller/AdminController.java
+++ src/main/java/app/controller/AdminController.java
@@ -1,12 +1,15 @@
 package app.controller;
 
+import app.dto.ProductDTO;
 import app.dto.UserDTO;
+import app.service.api.ProductService;
 import app.service.api.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -17,22 +20,21 @@ public class AdminController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private ProductService productService;
+
     @RequestMapping(value = "/adminPanel",method = RequestMethod.GET)
     public String adminPanel(){
         return "adminPanel";
     }
 
-    @RequestMapping(value = "/usersList" , method = RequestMethod.GET)
-    public String listUsers(ModelMap model) {
-        List<UserDTO> users = userService.getAll();
-        model.addAttribute("users", users);
-        return "usersList";
+    @RequestMapping(value = "/adminPanel/usersList" , method = RequestMethod.GET)
+    public @ResponseBody List<UserDTO> usersList() {
+        return userService.getAll();
     }
 
-    @RequestMapping(value = "/productsList", method = RequestMethod.GET)
-    public String productsList(ModelMap model) {
-        List<UserDTO> users = userService.getAll();
-        model.addAttribute("cars", users);
-        return "productsList";
+    @RequestMapping(value = "/adminPanel/productsList", method = RequestMethod.GET)
+    public @ResponseBody List<ProductDTO> productsList() {
+        return productService.getAll();
     }
 }
